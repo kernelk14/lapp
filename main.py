7d3ccd00e203eb706498827675d3935c968ff58a
kernelk14 <khyleisaias@gmail.com>
2022-03-08 20:44:07 +0800

    Parse a single string

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import re
 import os
 import sys
 filename = "si.txt"
-key = ''
-whitespace = ' '
+
 with open(filename, "r") as f:
     program = f.read()
 tokens = [' ', 'var', 'write']
@@ -18,18 +17,49 @@ nt = lex.read_token()
 sp = split(program)
 # print(sp)
 # print()
-for op, p in enumerate(program):
-    if key != whitespace:
-        print(key)
-        key = ''
-    if nt == '':
-        pass
-        # nt = lex.next_token()
+def write(op, sp, c, p, nt, program, gt):    
+    if sp[0] == 'write':            
+        lex.push_token(gt)
+        # a = lex.pop_token()
+        gt = lex.get_token()
+        print(c)
+        print(sp)
+        # print(sp[0+1])
+        # if sp 
+        # sp[c] = p
+        # print(a)
         # print(nt)
-        # print(gt)
-    else:
-        if sp[0] == 'write':
+        
             
-            lex.push_token(gt)
-            print(sp[0+1])
-            # break
+        # print(c - p)
+def sim(program):
+    key = ''
+    whitespace = ' '
+    lex = shlex(program)
+    gt = lex.get_token()
+    nt = lex.read_token()
+    sp = split(program)
+    c = 0
+    for p, op in enumerate(program):
+        if op != whitespace:
+            key += op
+        if (p + 1 < len(program)):
+            if program[p+1] == whitespace:
+                return write(op, sp, c, p, nt, program, gt)
+                # print(key)
+                c += 1
+                key = ''
+        if nt == '':
+            pass
+            c += 1
+            # nt = lex.next_token()
+            # print(nt)
+            # print(gt)
+        else:
+            if sp[0] == 'write':            
+                lex.push_token(gt)
+                # return write(op, sp, c, p, nt, program, gt)
+                print(sp[c])
+                c += 1
+                # break
+sim(program)
